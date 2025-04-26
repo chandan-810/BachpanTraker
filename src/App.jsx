f91350caff9ed66f4c6988ca1bde8826432814af
Lalit Yadav <140280rakesh@gmail.com>
2025-04-26 21:53:01 +0530

    Initial Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import Home from "./components/pages/Home";
+import Login from "./components/pages/Auth/Login";
+import Register from "./components/pages/Auth/Register";
 
 function App() {
   return (
-    <>
-      <div></div>
-    </>
+    <div>
+      <Routes>
+        <Route index element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+      </Routes>
+    </div>
   );
 }
 
