eff1efc5729ad20403d1383a96e7c36b27e93f2b
Chandan-810 <adhikarichandan122@gmail.com>
2025-04-27 21:23:13 +0530

    same change

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import { memo } from "react";
 
 const Button = () => {
-  return (
-    <div>
-      <div>Button</div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default memo(Button);
